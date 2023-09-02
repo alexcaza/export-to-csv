b14a6abc12c775c22cf1f9f97ec6a87038a4cc50
Alex Caza <alex@alexcaza.com>
2023-09-01 22:16:09 -0400

    Update karma
    now run new test for functional style API.
    
    Still need to add more test cases.

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,13 @@
-module.exports = function (config) {
+module.exports = function(config) {
     config.set({
-        basePath: './',
-        exclude: ['node_modules', '*.d.ts'],
-        files: ['*.ts', '*.spec.ts'],
+        basePath: "./",
+        exclude: ["node_modules", "*.d.ts"],
+        files: ["*.ts", "lib/*.ts", "*.spec.ts"],
         preprocessors: {
-            '**/*.ts': 'karma-typescript'
+            "**/*.ts": "karma-typescript",
         },
-        browsers: ['Chrome'],
-        frameworks: ['jasmine', 'karma-typescript'],
-        reporters: ['karma-typescript', 'progress']
+        browsers: ["Chrome"],
+        frameworks: ["jasmine", "karma-typescript"],
+        reporters: ["karma-typescript", "progress"],
     });
-}
+};
---
+++ main.spec.ts
@@ -0,0 +1,143 @@
+import { mkConfig } from "./lib/config";
+import { download, generateCsv } from "./lib/generator";
+import { unpack } from "./lib/helpers";
+import { ConfigOptions } from "./lib/types";
+
+const mockData = [
+  {
+    name: "Test 1",
+    age: 13,
+    average: 8.2,
+    approved: true,
+    description: "Test 1 description",
+  },
+  {
+    name: "Test 2",
+    age: 11,
+    average: 8.2,
+    approved: true,
+    description: "Test 2 description",
+  },
+  {
+    name: "Test 4",
+    age: 10,
+    average: 8.2,
+    approved: true,
+    description: "Test 3 description",
+  },
+];
+
+describe("ExportToCsv", () => {
+  it("should create a comma seperated string", () => {
+    const options: ConfigOptions = {
+      title: "Test Csv",
+      useBom: true,
+      useKeysAsHeaders: true,
+    };
+
+    const string = generateCsv(options)(mockData);
+    expect(typeof string === "string").toBeTrue();
+  });
+
+  it("should use keys of first object in collection as headers", () => {
+    const options: ConfigOptions = {
+      title: "Test Csv",
+      useBom: true,
+      useKeysAsHeaders: true,
+    };
+
+    const string = generateCsv(options)(mockData);
+
+    console.log("output:", string);
+
+    const firstLine = unpack(string).split("\n")[0];
+    const keys = firstLine.split(",").map((s: string) => s.trim());
+
+    const mockDataKeys = Object.keys(mockData[0]);
+    expect(keys).toEqual(mockDataKeys);
+  });
+
+  it("should initiate download through spawned browser", () => {
+    if (!window) {
+      pending("it should only initiate download when run in browser context");
+    }
+    const options: ConfigOptions = {
+      title: "Test Csv",
+      useBom: true,
+      useKeysAsHeaders: true,
+    };
+
+    const generator = generateCsv(options);
+    const downloader = download(options);
+
+    const output = generator(mockData);
+    downloader(output);
+  });
+
+  it("should retain order of headers when given as option", () => {
+    const options: ConfigOptions = {
+      filename: "Test Csv 2",
+      useBom: true,
+      showColumnHeaders: true,
+      columnHeaders: ["name", "average", "age", "approved", "description"],
+    };
+
+    const withDefaults = mkConfig(options);
+
+    const output = generateCsv(options)(mockData);
+
+    const firstLine = unpack(output).split("\n")[0];
+    const keys = firstLine.split(",").map((s: string) => s.trim());
+
+    expect(keys).toEqual(withDefaults.columnHeaders);
+  });
+});
+
+describe("ExportToCsv As A Text File", () => {
+  it("should create a comma seperated string", () => {
+    const options: ConfigOptions = {
+      title: "Test Csv 1",
+      useTextFile: true,
+      useBom: true,
+      showColumnHeaders: true,
+      useKeysAsHeaders: true,
+    };
+
+    const string = generateCsv(options)(mockData);
+    expect(typeof string === "string").toBeTrue();
+  });
+
+  it("should use keys of first object in collection as headers", () => {
+    const options: ConfigOptions = {
+      filename: "Test Csv 2",
+      useTextFile: true,
+      useBom: true,
+      showColumnHeaders: true,
+      useKeysAsHeaders: true,
+    };
+
+    const output = generateCsv(options)(mockData);
+
+    const firstLine = unpack(output).split("\n")[0];
+    const keys = firstLine.split(",").map((s: string) => s.trim());
+
+    const mockDataKeys = Object.keys(mockData[0]);
+    expect(keys).toEqual(mockDataKeys);
+  });
+
+  it("should initiate download through spawned browser", () => {
+    if (!window) {
+      pending("it should only initiate download when run in browser context");
+    }
+    const options: ConfigOptions = {
+      filename: "Test Csv 3",
+      useTextFile: true,
+      useBom: true,
+      showColumnHeaders: true,
+      useKeysAsHeaders: true,
+    };
+
+    const output = generateCsv(options)(mockData);
+    download(options)(output);
+  });
+});
