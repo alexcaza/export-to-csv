4a64bd5b2e9430c71475b933496e8857820f5673
Alex Caza <alex@alexcaza.com>
2023-09-02 21:33:50 -0400

    Create script to run e2e-server
    Also adds playwright launch

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
+    command: "npm run e2e-server",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
